d2afb14e4db1080cd5c29a00ee6da0fa471d076f
xiaocan <1217272889@qq.com>
2016-07-14 19:16:45 +0800

    none
    null

--- PlayerTest/ViewController.m
+++ PlayerTest/ViewController.m
@@ -7,16 +7,54 @@
 //
 
 #import "ViewController.h"
+#import <MediaPlayer/MediaPlayer.h>
+#import <AVKit/AVKit.h>
+#import <AVFoundation/AVFoundation.h>
+#import "XCAVPlayerView.h"
 
 @interface ViewController ()
+
+@property (nonatomic, strong) XCAVPlayerView *playerView;
 
 @end
 
 @implementation ViewController
 
+- (XCAVPlayerView *)playerView{
+    if (!_playerView) {
+        _playerView = [[XCAVPlayerView alloc]init];
+        [self.view addSubview:_playerView];
+    }
+    return _playerView;
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+
+    [[NSNotificationCenter defaultCenter]addObserver:self selector:@selector(statuesBarChanged:) name:UIApplicationDidChangeStatusBarOrientationNotification object:nil];
+    self.playerView.frame = CGRectMake(0, 20.0, [UIScreen mainScreen].bounds.size.width, [UIScreen mainScreen].bounds.size.width * 0.58);
+    self.playerView.playerUrl = [NSURL URLWithString:@"http://api.feixiong.tv/Api/Base/getShortM3u8?params=%7B%22data%22%3A%7B%22id%22%3A281%2C%22stream_type%22%3A%22hd2%22%2C%22ykss%22%3A%22%22%7D%7D"];
+    [self.playerView play];
+}
+
+
+
+- (void)statuesBarChanged:(NSNotification *)sender{
+    UIInterfaceOrientation statues = [UIApplication sharedApplication].statusBarOrientation;
+    if (statues == UIInterfaceOrientationPortrait || statues == UIInterfaceOrientationPortraitUpsideDown) {
+        self.playerView.frame = CGRectMake(0, 20.0, [UIScreen mainScreen].bounds.size.width, [UIScreen mainScreen].bounds.size.width * 0.58);
+    }else if (statues == UIInterfaceOrientationLandscapeLeft || statues == UIInterfaceOrientationLandscapeRight){
+        self.playerView.frame = CGRectMake(0, 0, [UIScreen mainScreen].bounds.size.width, [UIScreen mainScreen].bounds.size.height);
+    }
+}
+
+- (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary<NSString *,id> *)change context:(void *)context{
+    NSLog(@"%@--%@",object,[change description]);
+}
+
+- (void)moviePlayDidEnd:(NSNotification *)noti{
+    
 }
 
 - (void)didReceiveMemoryWarning {
